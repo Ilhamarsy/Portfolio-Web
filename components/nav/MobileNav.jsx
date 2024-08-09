493f3cb58671046237ca67ba9d6e10901c3dfb0a
Ilham Arsy <Ilhamarsy16@gmail.com>
2024-08-09 16:38:58 +0800

    fix mobile layout nav

--- components/nav/MobileNav.jsx
+++ components/nav/MobileNav.jsx
@@ -1,22 +1,23 @@
-"use client"
-import React, { useRef, useState } from "react";
+"use client";
+import React, { useEffect, useRef, useState } from "react";
 import { motion } from "framer-motion";
 import Link from "next/link";
+import { usePathname } from "next/navigation";
 
 const links = [
-    {
-      name: "Home",
-      path: "/",
-    },
-    {
-      name: "About",
-      path: "/about",
-    },
-    {
-      name: "Project",
-      path: "/project",
-    },
-  ];
+  {
+    name: "Home",
+    path: "/",
+  },
+  {
+    name: "About",
+    path: "/about",
+  },
+  {
+    name: "Project",
+    path: "/project",
+  },
+];
 
 export const MobileNav = () => {
   const [position, setPosition] = useState({
@@ -25,64 +26,75 @@ export const MobileNav = () => {
     opacity: 0,
   });
 
+  const pathname = usePathname(); 
+  const refs = useRef([]); 
+
+  useEffect(() => {
+    const activeLinkIndex = links.findIndex((link) => link.path === pathname);
+    if (activeLinkIndex !== -1 && refs.current[activeLinkIndex]) {
+      const activeLink = refs.current[activeLinkIndex];
+      const { width } = activeLink.getBoundingClientRect();
+      setPosition({ left: activeLink.offsetLeft, width, opacity: 1 });
+    }
+  }, [pathname]);
+
   return (
     <ul
       onMouseLeave={() => {
-        setPosition((pv) => ({
-          ...pv,
-          opacity: 0,
-        }));
+        const activeLinkIndex = links.findIndex((link) => link.path === pathname);
+        if (activeLinkIndex !== -1 && refs.current[activeLinkIndex]) {
+          const activeLink = refs.current[activeLinkIndex];
+          const { width } = activeLink.getBoundingClientRect();
+          setPosition({ left: activeLink.offsetLeft, width, opacity: 1 });
+        } else {
+          setPosition((prev) => ({
+            ...prev,
+            opacity: 0,
+          }));
+        }
       }}
       className="relative mx-auto flex w-fit rounded-full border-2 border-black bg-white p-1 drop-shadow-md"
     >
-        {links.map((link, index) => (
-            <Tab setPosition={setPosition} key={index} path={link.path}>{link.name}</Tab>
-        ))}
+      {links.map((link, index) => (
+        <Tab
+          setPosition={setPosition}
+          key={index}
+          path={link.path}
+          ref={(el) => (refs.current[index] = el)}
+          isActive={pathname === link.path}
+        >
+          {link.name}
+        </Tab>
+      ))}
 
       <Cursor position={position} />
     </ul>
   );
 };
 
-const Tab = ({ children, setPosition, path }) => {
-  const ref = useRef(null);
-
+const Tab = React.forwardRef(({ children, setPosition, path, isActive }, ref) => {
   return (
     <Link
-    ref={ref}
-          href={path}
-          onMouseEnter={() => {
-            if (!ref?.current) return;
-    
-            const { width } = ref.current.getBoundingClientRect();
-    
-            setPosition({
-              left: ref.current.offsetLeft,
-              width,
-              opacity: 1,
-            });
-          }}
-          className="relative z-10 block cursor-pointer px-3 py-1.5 text-xs uppercase text-white mix-blend-difference md:px-5 md:py-3 md:text-base"
-        >{children}</Link>
-    // <li
-    //   ref={ref}
-    //   onMouseEnter={() => {
-    //     if (!ref?.current) return;
+      ref={ref}
+      href={path}
+      onMouseEnter={() => {
+        if (!ref?.current) return;
 
-    //     const { width } = ref.current.getBoundingClientRect();
-
-    //     setPosition({
-    //       left: ref.current.offsetLeft,
-    //       width,
-    //       opacity: 1,
-    //     });
-    //   }}
-    //   className="relative z-10 block cursor-pointer px-3 py-1.5 text-xs uppercase text-white mix-blend-difference md:px-5 md:py-3 md:text-base"
-    // >
-    //   {children}
-    // </li>
+        const { width } = ref.current.getBoundingClientRect();
+        setPosition({
+          left: ref.current.offsetLeft,
+          width,
+          opacity: 1,
+        });
+      }}
+      className={`relative z-10 block cursor-pointer px-3 py-1.5 text-xs uppercase ${
+        isActive ? "text-white" : "text-white"
+      } mix-blend-difference md:px-5 md:py-3 md:text-base`}
+    >
+      {children}
+    </Link>
   );
-};
+});
 
 const Cursor = ({ position }) => {
   return (
@@ -90,7 +102,10 @@ const Cursor = ({ position }) => {
       animate={{
         ...position,
       }}
+      transition={{ duration: 0.3 }} 
       className="absolute z-0 h-7 rounded-full bg-black md:h-12"
     />
   );
 };
+
+Tab.displayName = "Tab"; 
