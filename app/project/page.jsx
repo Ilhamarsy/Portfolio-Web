59fb9dc71c082c98eda50f27ae5c844b5a880fd6
Ilham Arsy <Ilhamarsy16@gmail.com>
2024-08-12 09:48:07 +0800

    add shadescn components

--- app/project/page.jsx
+++ app/project/page.jsx
@@ -2,7 +2,8 @@ import React from 'react'
 
 function Project() {
   return (
-    <div>Project</div>
+    <div className="h-fit flex items-center justify-center pt-4 md:pt-0 md:py-4">
+      <div className="container mx-auto">Project</div></div>
   )
 }
 
